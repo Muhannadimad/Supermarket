51780376558a03646e96d69f69fda4d1e9d56b7d
Muhannadimad <61332284+Muhannadimad@users.noreply.github.com>
2020-09-17 00:55:40 +0300

    creat webapi template

---
+++ src/Supermarket.API/Controllers/CategoriesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Supermarket.API.Controllers
+{
+    [Route("/api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        
+    }
+}
